faa29d028918313f0375831e5a8fe45a1cd7e9d8
sainivas-yellapragada <sainivasyellapragada@gmail.com>
2025-03-04 19:26:03 +0530

    fixed settings3

--- Contactlist/settings.py
+++ Contactlist/settings.py
@@ -120,10 +120,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')  # This is where collectstatic will store files
+STATICFILES_DIRS = []  # No need for extra static files directory
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
